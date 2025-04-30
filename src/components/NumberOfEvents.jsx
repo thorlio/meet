7f399172c46570c3a7591ad2213eec0a2befa5f4
thorlio <thorlio3@gmail.com>
2025-04-29 20:48:15 -0700

    updated numberofevents

--- src/components/NumberOfEvents.jsx
+++ src/components/NumberOfEvents.jsx
@@ -5,8 +5,9 @@ const NumberOfEvents = ({ currentNOE, setCurrentNOE, setErrorAlert }) => {
   const [number, setNumber] = useState(currentNOE);
 
   const handleInputChanged = (event) => {
-    const value = event.target.value;
+    const value = Number(event.target.value);
     setNumber(value);
+
     if (isNaN(value) || value <= 0) {
       setErrorAlert("Enter a valid number");
     } else if (value > 32) {
