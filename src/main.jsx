b394b16c0b24aaf985e9bfe594206c89fa25783e
thorlio <thorlio3@gmail.com>
2025-05-08 01:43:01 -0700

    added atatus

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import * as atatus from "atatus-spa";
-
 atatus.config("5b8257f536db44b190ed7b16b57c2fa8").install();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
