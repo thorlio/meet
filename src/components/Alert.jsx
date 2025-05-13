57d33dc73b967b494be6ef20203724f4a9938227
thorlio <thorlio3@gmail.com>
2025-05-13 00:56:46 -0700

    added warning alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -50,4 +50,12 @@ class ErrorAlert extends Alert {
   }
 }
 
-export { Alert, InfoAlert, ErrorAlert };
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = "rgb(102, 85, 0)";
+    this.bgColor = "rgb(255, 235, 59)";
+  }
+}
+
+export { Alert, InfoAlert, ErrorAlert, WarningAlert };
